5554310f62924c638acc155688c81ef6cd9ceed2
Leon Jiang <nyjiang@uwaterloo.ca>
2017-11-09 17:49:56 -0500

    first commit

---
+++ lock.py
@@ -0,0 +1,24 @@
+from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor, Adafruit_StepperMotor
+
+import time
+import atexit
+from flask import Flask
+app = Flask(__name__)
+
+mh = Adafruit_MotorHAT(addr = 0x60)
+
+def turn_off_motor():
+  mh.getMotor(1).run(Adafruit_MotorHAT.RELEASE)
+  mh.getMotor(2).run(Adafruit_MotorHAT.RELEASE)
+  mh.getMotor(3).run(Adafruit_MotorHAT.RELEASE)
+  mh.getMotor(4).run(Adafruit_MotorHAT.RELEASE)
+
+myStepper = mh.getStepper(200, 1)
+myStepper.setSpeed(30)
+def step(n):
+  if n > 0:
+    dir = Adafruit_MotorHAT.BACKWARD
+  else:
+    dir = Adafruit_MotorHAT.FORWARD
+  myStepper.step(abs(n), dir, Adafruit_MotorHAT.DOUBLE)
+  turn_off_motor()
